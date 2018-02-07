2e8c9404fcc39edc779602128b9c1c1613eb94d5
mederic <mederic@omise.co>
2018-02-07 16:34:24 +0700

    Remove consume url for now

--- OmiseGOTests/ModelTests/TransactionRequestTest.swift
+++ OmiseGOTests/ModelTests/TransactionRequestTest.swift
@@ -17,8 +17,7 @@ class TransactionRequestTest: XCTestCase {
                                                     mintedTokenId: "BTC:9e3d1fc4-5899-4297-9a62-a9f524d5ca27",
                                                     amount: nil,
                                                     address: "1927dd11-8d05-42f8-8528-867d51c756c2",
-                                                    correlationId: "31009545-db10-4287-82f4-afb46d9741d8",
-                                                    consumeURL: "https://example.com/test")
+                                                    correlationId: "31009545-db10-4287-82f4-afb46d9741d8")
         if let qrImage = transactionRequest.qrImage() {
             let decodedText = QRTestHelper.readQRCode(fromImage: qrImage)
             XCTAssertEqual(decodedText, transactionRequest.id)
@@ -33,22 +32,19 @@ class TransactionRequestTest: XCTestCase {
                                                      mintedTokenId: "",
                                                      amount: nil,
                                                      address: "",
-                                                     correlationId: "",
-                                                     consumeURL: "")
+                                                     correlationId: "")
         let transactionRequest2 = TransactionRequest(id: "1",
                                                      type: .receive,
                                                      mintedTokenId: "",
                                                      amount: nil,
                                                      address: "",
-                                                     correlationId: "",
-                                                     consumeURL: "")
+                                                     correlationId: "")
         let transactionRequest3 = TransactionRequest(id: "2",
                                                      type: .receive,
                                                      mintedTokenId: "",
                                                      amount: nil,
                                                      address: "",
-                                                     correlationId: "",
-                                                     consumeURL: "")
+                                                     correlationId: "")
         XCTAssertEqual(transactionRequest1, transactionRequest2)
         XCTAssertNotEqual(transactionRequest1, transactionRequest3)
     }
@@ -59,15 +55,13 @@ class TransactionRequestTest: XCTestCase {
                                                      mintedTokenId: "",
                                                      amount: nil,
                                                      address: "",
-                                                     correlationId: "",
-                                                     consumeURL: "")
+                                                     correlationId: "")
         let transactionRequest2 = TransactionRequest(id: "1",
                                                      type: .receive,
                                                      mintedTokenId: "",
                                                      amount: nil,
                                                      address: "",
-                                                     correlationId: "",
-                                                     consumeURL: "")
+                                                     correlationId: "")
         let set: Set<TransactionRequest> = [transactionRequest1, transactionRequest2]
         XCTAssertEqual(transactionRequest1.hashValue, "1".hashValue)
         XCTAssertEqual(set.count, 1)
